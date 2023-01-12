724a3f226715b41f16adcd6cb4a223cf4cb0e3f0
Abdul Azis <abdul.azizz1108@gmail.com>
2023-01-12 10:26:40 +0700

    create get all books route
    create get all book route and create get all books hander

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,19 @@
-const routes = [];
+const {
+  addBookHandler,
+  getAllBooksHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+];
 
 module.exports = routes;
